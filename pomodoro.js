0806d9d7ea1567dfaeb7103d4ec8b0d96aa59ef9
Marcin Rudzki <mar.rudzki@gmail.com>
2018-03-02 12:47:53 +0100

    Add pause and stop buttons, add leading zeros

--- pomodoro.js
+++ pomodoro.js
@@ -1,29 +1,64 @@
 const POMODORO_STATES = {
-    WORK: '',
-    REST: ''
+    WORK: 'work',
+    REST: 'rest'
 };
-const WORKING_TIME_LENGTH_IN_MINUTES = 25;
-const RESTING_TIME_LENGTH_IN_MINUTES = 5;
+const STATES = {
+    STARTED: 'started',
+    STOPPED: 'stopped',
+    PAUSED: 'paused'
+};
+const WORKING_TIME_LENGTH_IN_MINUTES = 1;
+const RESTING_TIME_LENGTH_IN_MINUTES = 1;
 
 new Vue({
     el: '#app',
     data: {
+        state: STATES.STOPPED,
         minute: WORKING_TIME_LENGTH_IN_MINUTES,
         second: 0,
         pomodoroState: POMODORO_STATES.WORK,
         timeStamp: 0
     },
+    computed: {
+        title: function() {
+            return this.pomodoroState === POMODORO_STATES.WORK ? 'Pracuj!' : 'Odpoczywaj!'
+        },
+        min: function() {
+            if (this.minute < 10) {
+                return '0' + this.minute;
+            }
+            return this.minute;
+        },
+        sec: function() {
+            if (this.second < 10) {
+                return '0' + this.second;
+            }
+            return this.second;
+        }
+    },
     methods: {
         start: function() {
+            this.state = STATES.STARTED;
             this._thick();
             this.interval = setInterval(this._thick, 1000);
         },
+        pause: function() {
+            this.state = STATES.PAUSED;
+            clearInterval(this.interval);
+        },
+        stop: function() {
+            this.state = STATES.STOPPED;
+            clearInterval(this.interval);
+            this.pomodoroState = POMODORO_STATES.WORK;
+            this.minute = WORKING_TIME_LENGTH_IN_MINUTES;
+            this.second = 0;
+        },
         _thick: function() {
-            if (this.second > 0) {
+            if (this.second !== 0) {
                 this.second--;
                 return;
             }
-            if (this.minute > 0) {
+            if (this.minute !== 0) {
                 this.minute--;
                 this.second = 59;
                 return;
